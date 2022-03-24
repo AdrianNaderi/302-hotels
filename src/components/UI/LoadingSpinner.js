c64705f6035bc0e0fd40f77f4bc4f1276811f37b
EmilFrick <emil.a.frick@gmail.com>
2022-03-24 10:02:34 +0100

    preparing http redux

---
+++ src/components/UI/LoadingSpinner.js
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return;
+  <div class="lds-ripple">
+    <div></div>
+    <div></div>
+  </div>;
+};
+
+export default LoadingSpinner;
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchSlice from "./search-slice";
 import authSlice from "./auth-slice";
 import bookingSlice from "./booking-slice";
-
+import httpSlice from "./http-slice";
 
 const store = configureStore({
   reducer: {
     search: searchSlice.reducer,
     auth: authSlice.reducer,
     booking: bookingSlice.reducer,
+    http: httpSlice.reducer,
   },
 });
 
