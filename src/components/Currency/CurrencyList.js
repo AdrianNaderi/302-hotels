36b6738fa087d56e3a65c632ce82ad779ef6f522
OskarJosefsson <oskar.josefsson12@gmail.com>
2022-03-25 13:53:07 +0100

    currency css

--- src/components/Currency/CurrencyList.js
+++ src/components/Currency/CurrencyList.js
@@ -23,33 +23,60 @@ const CurrencyList = (props) => {
 
   return (
     <React.Fragment>
-      <div className={classes.card}>
-        <img src="../../assets/Currency.webp"></img>
-        {
-          <div className={classes.container}>
-            <h1 className={classes.hOne}>Currencies</h1>
+      <div className="container">
+        <div className={classes.card}>
+          <div className={`bg-dark bg-opacity-50 ${classes.test}`}>
+            {
+              <div className={`row col-12 ${classes.currencytitle}`}>
+                <div className={`row col-12 ${classes.currency}`}>
+                  <h2>Currencies</h2>
+                </div>
+                <div className={"row"}>
+                  {" "}
+                  <div className={`col-6 ${classes.extracurrency}`}>
+                    {" "}
+                    <h2>
+                      EURO: ={" "}
+                      {(1 / props.currency.conversion_rates.EUR).toFixed(3)}
+                      {props.currency.base_code}
+                    </h2>
+                    <h2>
+                      CNY: ={" "}
+                      {(1 / props.currency.conversion_rates.CNY).toFixed(3)}
+                      {props.currency.base_code}
+                    </h2>
+                    <hr />
+                  </div>
+                  <div className={`col-6 ${classes.extracurrency}`}>
+                    <h2>
+                      USD: ={" "}
+                      {(1 / props.currency.conversion_rates.USD).toFixed(3)}
+                      {props.currency.base_code}
+                    </h2>
+                    <h2>
+                      GBP: ={" "}
+                      {(1 / props.currency.conversion_rates.GBP).toFixed(3)}
+                      {props.currency.base_code}
+                    </h2>
+                    <hr />
+                  </div>
+                </div>
 
-            <h2 className={classes.animationText}>
-              EURO: = {(1 / props.currency.conversion_rates.EUR).toFixed(3)}
-              {props.currency.base_code}
-              CNY: = {(1 / props.currency.conversion_rates.CNY).toFixed(3)}
-              {props.currency.base_code}
-              USD: = {(1 / props.currency.conversion_rates.USD).toFixed(3)}
-              {props.currency.base_code}
-              GBP: = {(1 / props.currency.conversion_rates.GBP).toFixed(3)}
-              {props.currency.base_code}
-            </h2>
-            <h2 className={classes.hTwo}></h2>
-            {!isDuplicate && (
-              <h2 className={classes.hTwo}>
-                {props.hotellCurr.target_code} <span> = </span>
-                {(1 / props.hotellCurr.conversion_rate).toFixed(3)}
-                {props.currency.base_code}
-              </h2>
-            )}
+                <div className={` row col-12 ${classes.extracurrency}`}>
+                  <h2 className={classes.hTwo}></h2>
+                  {!isDuplicate && (
+                    <h2>
+                      {props.hotellCurr.target_code} <span> = </span>
+                      {(1 / props.hotellCurr.conversion_rate).toFixed(3)}
+                      {props.currency.base_code}
+                      <hr />
+                    </h2>
+                  )}
+                </div>
+              </div>
+            }
           </div>
-        }
-        s
+        </div>
       </div>
     </React.Fragment>
   );
--- src/components/Weather/SearchWeather.js
+++ src/components/Weather/SearchWeather.js
@@ -2,108 +2,116 @@ import React, { useEffect, useState } from "react";
 import classes from "./SearchWeather.module.css";
 
 const SearchWeather = (props) => {
-    const [search, setSearch] = useState("london");
-    const [data, setData] = useState([]);
-    const [input, setInput] = useState("");
-    let componentMounted = true;
-  
+  const [search, setSearch] = useState("london");
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  let componentMounted = true;
 
-
-    useEffect(() => {
-        const fetchWheater = async () => {
-            try {
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&APPID=98aae41324ef9a2c804b4b4cb98b99fb`);
-                if(componentMounted){
-                    setData(await response.json());
-                    
-                }
-                return () => {
-                    componentMounted = false;
-                }
-                
-            } catch (err) {
-                alert(err);
-            }
-           
+  useEffect(() => {
+    const fetchWheater = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&APPID=98aae41324ef9a2c804b4b4cb98b99fb`
+        );
+        if (componentMounted) {
+          setData(await response.json());
         }
-        fetchWheater();
-    }, [props.city, componentMounted]);
-
-    let emoji = null;
-    if (typeof data.main != "undefined"){
-        if(data.weather[0].main == "Clouds"){
-            emoji = "fa-cloud"
-        }else if(data.weather[0].main == "Thunderstorm"){
-            emoji = "fa-bolt"
-        }else if(data.weather[0].main == "Drizzle"){
-            emoji = "fa-cloud-rain"
-        }else if(data.weather[0].main == "Rain"){
-            emoji = "fa-cloud-showers-heavy"
-        }else if(data.weather[0].main == "Snow"){
-            emoji = "fa-snow-flake"
-        }else {
-            emoji = "fa-smog"
-        }
-    }else {
-        return (
-            <div>...Loading</div>
-        )
-    }
-
-    let temp = (data.main.temp - 273.15).toFixed(2);
-    let temp_min = (data.main.temp_min - 273.15).toFixed(2);
-    let temp_max = (data.main.temp_max - 273.15).toFixed(2);
-
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+        return () => {
+          componentMounted = false;
+        };
+      } catch (err) {
+        alert(err);
       }
+    };
+    fetchWheater();
+  }, [props.city, componentMounted]);
 
-    let d = new Date();
-    let date = d.getDate();
-    let year = d.getFullYear();
-    let month = capitalizeFirstLetter(d.toLocaleString("default", {month: 'long'}));
-    let day = capitalizeFirstLetter(d.toLocaleString("default", {weekday: 'long'}));
+  let emoji = null;
+  if (typeof data.main != "undefined") {
+    if (data.weather[0].main == "Clouds") {
+      emoji = "fa-cloud";
+    } else if (data.weather[0].main == "Thunderstorm") {
+      emoji = "fa-bolt";
+    } else if (data.weather[0].main == "Drizzle") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main == "Rain") {
+      emoji = "fa-cloud-showers-heavy";
+    } else if (data.weather[0].main == "Snow") {
+      emoji = "fa-snow-flake";
+    } else {
+      emoji = "fa-smog";
+    }
+  } else {
+    return <div>...Loading</div>;
+  }
 
-   const handleSubmit = (event) => {
-       event.preventDefault();
-       setSearch(input);
-   }
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
 
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-    return (
-      <div>
-        <div className="container mt-5">
-          <div>
-            <div className={classes.fit}>
-              <div className={`card bg-dark text-white text-left border-0`}>
-                <img src={`https://source.unsplash.com/1200x400/?${data.weather[0].main}`} className={classes.cardImg} alt="..." />
-                <div className="card-img-overlay">
-                  <div className={`bg-dark bg-opacity-50 py-3 ${classes.test}`}>
-                    <div className="wrapper">
-                      <h2 className="card-title">
-                        {props.city}
-                        <span className={classes.span1}>
-                          <i className={`fas ${emoji} fa-4x`}></i>
-                        </span>
-                      </h2>
-                      <h4 className="card-title">
-                        <div className="row">
-                          <div className="col-6">
-                            <p>{props.country}</p>
-                            <p className={`card-text lead ${classes.date}`}>
-                        {day}, {month} {date}, {year} </p>
-                        <hr/>
-                          </div>
-                          <div className="col-5">
-                            <p className={`text-end ${classes.temperature}`}>{temp}&deg;C</p>
-                            <p className={`lead fw-bolder text-end mb-0 ${classes.weather}`}>{data.weather[0].main}</p>
-                            <p className={`lead text-end ${classes.minmaxtemp}`}>
-                              {temp_min}&deg;C | {temp_max}&deg;C
-                            </p>
-                          </div>
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = capitalizeFirstLetter(
+    d.toLocaleString("default", { month: "long" })
+  );
+  let day = capitalizeFirstLetter(
+    d.toLocaleString("default", { weekday: "long" })
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+  };
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div>
+          <div className={classes.fit}>
+            <div className={`card bg-dark text-white text-left border-0`}>
+              <img
+                src={`https://source.unsplash.com/1200x400/?${data.weather[0].main}`}
+                className={classes.cardImg}
+                alt="..."
+              />
+              <div className="card-img-overlay">
+                <div className={`bg-dark bg-opacity-50 py-3 ${classes.test}`}>
+                  <div className="wrapper">
+                    <h2 className="card-title">
+                      {props.city}
+                      <span className={classes.span1}>
+                        <i className={`fas ${emoji} fa-4x`}></i>
+                      </span>
+                    </h2>
+                    <h4 className="card-title">
+                      <div className="row">
+                        <div className="col-6">
+                          <p>{props.country}</p>
+                          <p className={`card-text lead ${classes.date}`}>
+                            {day}, {month} {date}, {year}{" "}
+                          </p>
+                          <hr />
                         </div>
-                      </h4>
-                    </div>
+                        <div className="col-5">
+                          <p className={`text-end ${classes.temperature}`}>
+                            {temp}&deg;C
+                          </p>
+                          <p
+                            className={`lead fw-bolder text-end mb-0 ${classes.weather}`}
+                          >
+                            {data.weather[0].main}
+                          </p>
+                          <p className={`lead text-end ${classes.minmaxtemp}`}>
+                            {temp_min}&deg;C | {temp_max}&deg;C
+                          </p>
+                        </div>
+                      </div>
+                    </h4>
                   </div>
                 </div>
               </div>
@@ -111,10 +119,8 @@ const SearchWeather = (props) => {
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
+};
 
-
- 
-}
-
-export default SearchWeather
+export default SearchWeather;
