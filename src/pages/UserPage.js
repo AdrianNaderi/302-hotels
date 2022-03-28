a3780a948becc457bb49b7c4fa25e2d9462ebe10
Adrian Naderi <Adrian.Naderi@hotmail.se>
2022-03-28 21:17:58 +0200

    UserPage layout

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,7 +1,27 @@
+import Input from "../components/UI/Input";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 const UserPage = (props) => {
+  const userName = useSelector((state) => state.auth.username);
+  const fullName = useSelector((state) => state.auth.name);
+  const navigate = useNavigate();
+
   return (
     <div className="container-fluid">
       <h1>My Page</h1>
+      <div className="container">
+        <Input />
+        <Input />
+        <Input />
+        <button
+          onClick={() => {
+            navigate("/bookings");
+          }}
+        >
+          My Bookings
+        </button>
+      </div>
     </div>
   );
 };
