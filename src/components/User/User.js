295f08df43f35d47b346ca93f9bc7a43e3c75227
Adrian Naderi <Adrian.Naderi@hotmail.se>
2022-03-29 00:16:27 +0200

    MyPage layout Css added

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,6 @@
 import Input from "../UI/Input";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import classes from "./User.module.css";
 
 const User = (props) => {
@@ -10,32 +10,33 @@ const User = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="container-fluid m-5">
-      <div className="d-flex flex-column justify-content-center">
-        <div className={`container ${classes.box}`}>
-          <div className="p-2 col-md-6">
-            <Input value={user} disabled={true} label="User Name" />
-            <Input value={fullName} disabled={true} label="Full Name" />
-          </div>
-          <div className="p-2 col-md-6">
-            <Input value="Street Adress XX" disabled={true} label="Address" />
-            <Input value="customer@domain.com" disabled={true} label="Email" />
-          </div>
-          <div className="p-2 col-md-6">
-            <Input value="xxx-xxxxxxx" disabled={true} label="Phone Number" />
-            <Input value="**********" disabled={true} label="Password" />
-          </div>
+    <div className="container mt-4 text-center">
+      <div className={classes.box}>
+        <div className="p-2">
+          <Input value={user} disabled={true} label="User Name" />
+          <Input value={fullName} disabled={true} label="Full Name" />
+        </div>
+        <div className="p-2">
+          <Input value="Street Adress XX" disabled={true} label="Address" />
+          <Input value="customer@domain.com" disabled={true} label="Email" />
+        </div>
+        <div className="p-2">
+          <Input value="xxx-xxxxxxx" disabled={true} label="Phone Number" />
+          <Input value="**********" disabled={true} label="Password" />
+        </div>
+        <div className="text-start">
+          <button
+            className={classes.button}
+            onClick={() => {
+              navigate("/bookings");
+            }}
+          >
+            My Bookings
+          </button>
+
+          <button className={classes.button}>Change Info</button>
         </div>
       </div>
-      <button
-        className={classes.button}
-        onClick={() => {
-          navigate("/bookings");
-        }}
-      >
-        My Bookings
-      </button>
-      <button className={classes.button}>Change Info</button>
     </div>
   );
 };
